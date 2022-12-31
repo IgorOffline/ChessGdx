0334bd612f7e03f14f71c99df858989a88441a1d
Igor Durbek <igor@igordurbek.com>
2022-12-31 22:45:44 +0100

    prune preparation 2

--- src/main/java/practice/Pruning.kt
+++ src/main/java/practice/Pruning.kt
@@ -1,6 +1,8 @@
 package practice
 
+import practice.board.Piece
 import practice.board.PieceColor
+import practice.piece.Rook
 
 class Pruning {
 
@@ -8,6 +10,19 @@ class Pruning {
 
         fun prune(gameMaster: GameMaster, pieceColor: PieceColor) {
 
+            val oppositePieceColor = if (pieceColor == PieceColor.WHITE) PieceColor.BLACK else PieceColor.WHITE
+
+            gameMaster.board.board.forEach { square ->
+                if (square.pieceColor == oppositePieceColor && square.piece == Piece.ROOK) {
+                    val rookMoves = Rook.rookMoves(square, gameMaster.board)
+                    if (rookMoves.opponentsKingInCheck) {
+                        kingStillInCheck(gameMaster, pieceColor)
+                    }
+                }
+            }
+        }
+
+        private fun kingStillInCheck(gameMaster: GameMaster, pieceColor: PieceColor) {
             when (pieceColor) {
                 PieceColor.WHITE -> {
                     gameMaster.whiteKingInCheck = true
